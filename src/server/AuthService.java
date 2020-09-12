b453e03d6b56ed1d000c2a78a912d6865baa1814
SergeyGrishchuk <sirgeyaid666@gmail.com>
2020-09-12 15:53:33 +0300

    Initial commit

---
+++ src/server/AuthService.java
@@ -0,0 +1,9 @@
+package server;
+
+public interface AuthService {
+    /**
+     * @return nickname если пользователь есть
+     * @return null если пользоватаеля нет
+     * */
+    String getNicknameByLoginAndPassword(String login, String password);
+}
